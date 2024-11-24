ae24b13252b9ce38cd4d10ae3176582a28735134
Juan Guevara <soyjuandavidguevara@gmail.com>
2024-11-24 11:15:33 -0500

    removed unnecessary controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 import { DiagnosisModule } from './diagnosis/diagnosis.module';
-import { PatientDoctorService } from './patient-doctor/patient-doctor.service';
 import { PatientDoctorModule } from './patient-doctor/patient-doctor.module';
 import { TypeORMConfig } from './common/TypeORMConfig';
 
@@ -16,7 +13,5 @@ import { TypeORMConfig } from './common/TypeORMConfig';
     PatientDoctorModule,
     ...TypeORMConfig(),
   ],
-  controllers: [AppController],
-  providers: [AppService, PatientDoctorService],
 })
 export class AppModule {}
